317d8fc83e098723540ac493a1fed4d6cf4b3220
starkvr1 <prateekpatriotasme@gmail.com>
2022-03-05 16:16:18 +0530

    initial commit

--- snake-game/src/(default package)/gamePAnel.java
+++ snake-game/src/(default package)/gamePAnel.java
@@ -45,10 +45,12 @@ public class gamePAnel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
         if (running) {
-            // for (int i =0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
-            // g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-            // g.drawLine(0, i*UNIT_SIZE, SCREEN_HEIGHT, i*UNIT_SIZE);
-            // }
+            // USE THEM FOR GRIDS:----------------------
+
+// for (int i =0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+// g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+// g.drawLine(0, i*UNIT_SIZE, SCREEN_HEIGHT, i*UNIT_SIZE);
+// }
             g.setColor(Color.red);
             // Changes the size of apple
             g.fillOval(appleX, appleY, UNIT_SIZE - 2, UNIT_SIZE - 2);
